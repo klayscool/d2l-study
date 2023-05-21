0cc3f730db02797692c7cfcaa08885cfcd8ffd26
yuehuazhang <yuehuazhang@tencent.com>
2023-05-22 01:03:04 +0800

    feat(64):注意力机制.

---
+++ 64注意力机制/main.py
@@ -0,0 +1,145 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+# 10.2.1. 生成数据集
+n_train = 50  # 训练样本数
+x_train, _ = torch.sort(torch.rand(n_train) * 5)   # 排序后的训练样本
+print('x_train:', x_train)
+
+def f(x):
+    return 2 * torch.sin(x) + x**0.8
+
+y_train = f(x_train) + torch.normal(0.0, 0.5, (n_train,))  # 训练样本的输出
+x_test = torch.arange(0, 5, 0.1)  # 测试样本
+y_truth = f(x_test)  # 测试样本的真实输出
+n_test = len(x_test)  # 测试样本数
+print(n_test)
+
+def plot_kernel_reg(y_hat):
+    d2l.plot(x_test, [y_truth, y_hat], 'x', 'y', legend=['Truth', 'Pred'],
+             xlim=[0, 5], ylim=[-1, 5])
+    d2l.plt.plot(x_train, y_train, 'o', alpha=0.5);
+
+# 10.2.2. 平均汇聚
+y_hat = torch.repeat_interleave(y_train.mean(), n_test)
+plot_kernel_reg(y_hat)
+
+# 10.2.3. 非参数注意力汇聚
+# X_repeat的形状:(n_test,n_train),
+# 每一行都包含着相同的测试输入（例如：同样的查询）
+print('x_train:', x_train)
+print('x_train.shape:', x_train.shape)
+X_repeat = x_test.repeat_interleave(n_train).reshape((-1, n_train))
+print('X_repeat:', X_repeat)
+print('X_repeat.shape:', X_repeat.shape)
+
+print('(X_repeat - x_train):', (X_repeat - x_train))
+print('(X_repeat - x_train).shape:', (X_repeat - x_train).shape)
+
+# print('(-(X_repeat - x_train)**2 / 2, dim=1):', (-(X_repeat - x_train)**2 / 2, dim=1))
+# print('(-(X_repeat - x_train)**2 / 2, dim=1).shape:', (-(X_repeat - x_train)**2 / 2, dim=1).shape)
+# x_train包含着键。attention_weights的形状：(n_test,n_train),
+# 每一行都包含着要在给定的每个查询的值（y_train）之间分配的注意力权重
+attention_weights = nn.functional.softmax(-(X_repeat - x_train)**2 / 2, dim=1)
+print('attention_weights:', attention_weights)
+print('attention_weights.shape:', attention_weights.shape)
+print('attention_weights.sum(axis=1):', attention_weights.sum(axis=1))
+
+# y_hat的每个元素都是值的加权平均值，其中的权重是注意力权重
+print('y_train:', y_train)
+print('y_train.shape:', y_train.shape)
+y_hat = torch.matmul(attention_weights, y_train)
+print('y_hat:', y_hat)
+print('y_hat.shape:', y_hat.shape)
+plot_kernel_reg(y_hat)
+
+d2l.show_heatmaps(attention_weights.unsqueeze(0).unsqueeze(0),
+                  xlabel='Sorted training inputs',
+                  ylabel='Sorted testing inputs')
+
+
+# 10.2.4. 带参数注意力汇聚
+# 10.2.4.1. 批量矩阵乘法
+X = torch.ones((2, 1, 4))
+Y = torch.ones((2, 4, 6))
+print(torch.bmm(X, Y).shape)
+
+weights = torch.ones((2, 10)) * 0.1
+print('weights:', weights)
+print('weights.shape:', weights.shape)
+print('weights.unsqueeze(1):', weights.unsqueeze(1))
+print('weights.unsqueeze(1).shape:', weights.unsqueeze(1).shape)
+
+values = torch.arange(20.0).reshape((2, 10))
+print('values:', values)
+print('values.shape:', values.shape)
+print('values.unsqueeze(-1)):', values.unsqueeze(-1))
+print('values.unsqueeze(-1)).shape:', values.unsqueeze(-1).shape)
+
+print('torch.bmm(weights.unsqueeze(1), values.unsqueeze(-1)):', torch.bmm(weights.unsqueeze(1), values.unsqueeze(-1)))
+print('torch.bmm(weights.unsqueeze(1), values.unsqueeze(-1)).shape:', torch.bmm(weights.unsqueeze(1), values.unsqueeze(-1)).shape)
+
+# 10.2.4.2. 定义模型
+class NWKernelRegression(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.w = nn.Parameter(torch.rand((1,), requires_grad=True))
+
+    def forward(self, queries, keys, values):
+        # queries和attention_weights的形状为(查询个数，“键－值”对个数)
+        queries = queries.repeat_interleave(keys.shape[1]).reshape((-1, keys.shape[1]))
+        self.attention_weights = nn.functional.softmax(
+            -((queries - keys) * self.w)**2 / 2, dim=1)
+        # values的形状为(查询个数，“键－值”对个数)
+        return torch.bmm(self.attention_weights.unsqueeze(1),
+                         values.unsqueeze(-1)).reshape(-1)
+
+# 10.2.4.3. 训练
+# X_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输入
+X_tile = x_train.repeat((n_train, 1))
+# Y_tile的形状:(n_train，n_train)，每一行都包含着相同的训练输出
+Y_tile = y_train.repeat((n_train, 1))
+# keys的形状:('n_train'，'n_train'-1)
+keys = X_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+# values的形状:('n_train'，'n_train'-1)
+values = Y_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+
+net = NWKernelRegression()
+loss = nn.MSELoss(reduction='none')
+trainer = torch.optim.SGD(net.parameters(), lr=0.5)
+animator = d2l.Animator(xlabel='epoch', ylabel='loss', xlim=[1, 5])
+
+for epoch in range(5):
+    trainer.zero_grad()
+    l = loss(net(x_train, keys, values), y_train)
+    l.sum().backward()
+    trainer.step()
+    print(f'epoch {epoch + 1}, loss {float(l.sum()):.6f}')
+    animator.add(epoch + 1, float(l.sum()))
+
+# keys的形状:(n_test，n_train)，每一行包含着相同的训练输入（例如，相同的键）
+print('x_test:', x_test)
+print('x_test.shape:', x_test.shape)
+
+print('x_train:', x_train)
+print('x_train.shape:', x_train.shape)
+
+keys = x_train.repeat((n_test, 1))
+print('keys:', keys)
+print('keys.shape:', keys.shape)
+
+# value的形状:(n_test，n_train)
+print('y_train:', x_train)
+print('y_train.shape:', x_train.shape)
+
+values = y_train.repeat((n_test, 1))
+print('values:', values)
+print('values.shape:', values.shape)
+
+y_hat = net(x_test, keys, values).unsqueeze(1).detach()
+plot_kernel_reg(y_hat)
+
+d2l.show_heatmaps(net.attention_weights.unsqueeze(0).unsqueeze(0),
+                  xlabel='Sorted training inputs',
+                  ylabel='Sorted testing inputs')
